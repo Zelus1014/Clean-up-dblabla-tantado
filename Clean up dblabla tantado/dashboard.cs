73130c0ea4975fc50a58c2e4a974ae7b8da2dd42
Mizuki-06 <nakamuramizuki12@gmail.com>
2025-12-27 13:27:33 +0800

    ahhhh

--- Clean up dblabla tantado/dashboard.cs
+++ Clean up dblabla tantado/dashboard.cs
@@ -28,41 +28,90 @@ namespace Clean_up_dblabla_tantado
         private void button6_Click(object sender, EventArgs e)
         {
             panel19.Visible = true;
+            cdata.Hide();
+            mc.Hide();
+            trash.Hide();
+            vol.Hide();
+            admin.Hide();
+            settings.Hide();
+            help.Hide();
         }
 
         private void button17_Click(object sender, EventArgs e)
         {
             cdata.Show();
+            mc.Hide();
+            trash.Hide();
+            vol.Hide();
+            admin.Hide();
+            settings.Hide();
+            help.Hide();
         }
 
         private void button18_Click(object sender, EventArgs e)
         {
             mc.Show();
+            cdata.Hide();
+            trash.Hide();
+            vol.Hide();
+            admin.Hide();
+            settings.Hide();
+            help.Hide();
         }
 
         private void button19_Click(object sender, EventArgs e)
         {
             trash.Show();
+            cdata.Hide();
+            mc.Hide();
+            vol.Hide();
+            admin.Hide();
+            settings.Hide();
+            help.Hide();
         }
 
         private void button20_Click(object sender, EventArgs e)
         {
-            vol.Show(); 
+            vol.Show();
+            cdata.Hide();
+            mc.Hide();
+            trash.Hide();
+            admin.Hide();
+            settings.Hide();
+            help.Hide();
         }
 
         private void button21_Click(object sender, EventArgs e)
         {
             admin.Show();
+            cdata.Hide();
+            mc.Hide();
+            trash.Hide();
+            vol.Hide();
+            settings.Hide();
+            help.Hide();
         }
 
         private void button22_Click(object sender, EventArgs e)
         {
             settings.Show();
+            cdata.Hide();
+            mc.Hide();
+            trash.Hide();
+            vol.Hide();
+            admin.Hide();
+            help.Hide();
         }
 
         private void button30_Click(object sender, EventArgs e)
         {
             help.Show();
+            cdata.Hide();
+            mc.Hide();
+            trash.Hide();
+            vol.Hide();
+            admin.Hide();
+            settings.Hide();
         }
     }
 }
